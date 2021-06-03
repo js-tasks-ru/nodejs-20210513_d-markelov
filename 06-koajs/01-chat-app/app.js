ca406a9dee2482892c308d7c4ec3f53ca50f4043
dmark <d.markelov@glovisrus.com>
2021-06-03 16:26:31 +0300

    solution for 06.01 chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,45 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  let message;
+  const promise = new Promise((resolved, failed) => {
+    subscribers.push(resolved);
+
+    ctx.res.on('close', function() {
+      subscribers.splice(subscribers.indexOf(resolved), 1);
+      const error = new Error('Connection closed');
+      error.code = 'ECONNRESET';
+      failed(error);
+    });
+  });
+
+  try {
+    message = await promise;
+  } catch (e) {
+    if (e.code === 'ECONNRESET') return;
+    throw e;
+  }
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const msg = ctx.request.body.message;
+
+  if (!msg) {
+    ctx.throw(400, 'message required');
+  }
+
+  subscribers.forEach(function(resolve) {
+    resolve(String(msg));
+  });
+
+  subscribers = [];
+
+  ctx.body = 'published';
 });
 
 app.use(router.routes());
